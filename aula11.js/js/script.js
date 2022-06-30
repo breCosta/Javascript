d0c28d15613c5bbbfec928bcf248827c34312e26
Brenda Louise Medeiros de Figueiredo Costa <67911648+breCosta@users.noreply.github.com>
2022-06-30 10:01:58 -0300

    Add files via upload

---
+++ aula11.js/js/script.js
@@ -0,0 +1,9 @@
+let num1 = prompt('Digite o primeiro numero: ');
+let num2 = prompt('Digite outro numero: ');
+let soma;
+
+num1 = Number(num1);
+num2 = Number(num2);
+soma = num1 + num2;
+
+alert(`A soma é ${soma}`);
